0cb3f9a3f50b6218fb72ca69c9e37cc656e758f0
Thomas Haymore <thomas.haymore@gmail.com>
2012-11-08 19:00:32 -0800

    switched to decorator that returns function, not bool

--- prosperime/entities/management/commands/_retry.py
+++ prosperime/entities/management/commands/_retry.py
@@ -1,41 +1,47 @@
 import time
-import math
+import urllib2
+import simplejson
 
-# Retry decorator with exponential backoff
-def retry(tries, delay=3, backoff=2):
-  '''Retries a function or method until it returns True.
 
-  delay sets the initial delay in seconds, and backoff sets the factor by which
-  the delay should lengthen after each failure. backoff must be greater than 1,
-  or else it isn't really a backoff. tries must be at least 0, and delay
-  greater than 0.'''
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
 
-  if backoff <= 1:
-    raise ValueError("backoff must be greater than 1")
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
 
-  tries = math.floor(tries)
-  if tries < 0:
-    raise ValueError("tries must be 0 or greater")
-
-  if delay <= 0:
-    raise ValueError("delay must be greater than 0")
-
-  def deco_retry(f):
-    def f_retry(*args, **kwargs):
-      mtries, mdelay = tries, delay # make mutable
-
-      rv = f(*args, **kwargs) # first attempt
-      while mtries > 0:
-        if rv is True: # Done on success
-          return True
-
-        mtries -= 1      # consume an attempt
-        time.sleep(mdelay) # wait...
-        mdelay *= backoff  # make future wait longer
-
-        rv = f(*args, **kwargs) # Try again
-
-      return False # Ran out of tries :-(
-
-    return f_retry # true decorator -> decorated function
-  return deco_retry  # @retry(arg[, ...]) -> true decorator
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        excpetions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            try_one_last_time = True
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                    try_one_last_time = False
+                    break
+                except ExceptionToCheck, e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print msg
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            if try_one_last_time:
+                return f(*args, **kwargs)
+            return
+        return f_retry  # true decorator
+    return deco_retry
