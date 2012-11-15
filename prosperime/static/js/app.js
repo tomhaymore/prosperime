f3f3c4525012b8bb8fdc8c1344db22bea5b979be
Thomas Haymore <thomas.haymore@gmail.com>
2012-11-14 21:14:49 -0800

    added static files

---
+++ prosperime/static/js/app.js
@@ -0,0 +1,11 @@
+// Models
+
+var doc = Backbone.Model.extend();
+
+var docCollection = Backbone.Collection.extend();
+
+// Views
+
+var docSingleView = Backbone.View.extend();
+
+var docListView = Backbone.View.extend();
