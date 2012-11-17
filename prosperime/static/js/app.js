33f4f96ab802a8139f961c1ebb566678d0f166a1
Thomas Haymore <thomas.haymore@gmail.com>
2012-11-17 15:02:09 -0800

    static files, js, img, css

--- prosperime/static/js/app.js
+++ prosperime/static/js/app.js
@@ -1,11 +1,48 @@
 // Models
 
-var doc = Backbone.Model.extend();
+var Org = Backbone.Model.extend();
 
-var docCollection = Backbone.Collection.extend();
+var OrgList = Backbone.Collection.extend({
+
+	model: Org,
+
+
+});
 
 // Views
 
-var docSingleView = Backbone.View.extend();
+var OrgSingleView = Backbone.View.extend(
 
-var docListView = Backbone.View.extend();
+	tagName: "li",
+
+	template:_.template($('#org-template').html()),
+
+	initialize: function() {
+		this.model.bind('change',this.render,this);
+		this.model.bind('destroy',this.remove,this);
+	},
+
+	events:{
+		"click .view" 		: "fullDisplay",
+		"click .dismiss" 	: "dismiss"
+	},
+
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+	},
+
+	display: function() {
+
+	},
+
+	dismiss: function() {
+
+	},
+
+	schema: {
+		location: {type: }
+	}
+
+	);
+
+var OrgListView = Backbone.View.extend();
