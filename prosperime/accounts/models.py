49c9c05bb599ac8c359ade325e3a9398649ac151
Thomas Haymore <thomas.haymore@gmail.com>
2012-12-03 20:15:19 -0800

    -- added headline field to profile model, also callback for generating full name

--- prosperime/accounts/models.py
+++ prosperime/accounts/models.py
@@ -7,11 +7,18 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 class Profile(models.Model):
+	
+	# returns path for uploading logos
+	def _getFullName(self):
+		full_name = self.first_name + " " + self.last_name
+		return full_name
+
 	user = models.OneToOneField(User)
-	full_name = models.CharField(max_length=250,null=True)
+	full_name = models.CharField(max_length=250,null=True,default=_getFullName)
 	first_name = models.CharField(max_length=150,null=True)
 	middle_name = models.CharField(max_length=150,null=True)
 	last_name = models.CharField(max_length=150,null=True)
+	headline = models.TextField(null=True)
 
 class Account(models.Model):
     service = models.CharField(max_length=45)
