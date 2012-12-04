1128c81937a07ef37722b4dc572cbfe33fd1d79b
Thomas Haymore <thomas.haymore@gmail.com>
2012-12-03 20:14:51 -0800

    -- created FinishAuth form

---
+++ prosperime/accounts/forms.py
@@ -0,0 +1,8 @@
+# from Django
+from django import forms
+
+class FinishAuthForm(forms.Form):
+	username = forms.CharField(label="Username")
+	email = forms.EmailField(label="Email")
+	password = forms.CharField(widget=forms.PasswordInput,label="Password")
+	confirm_password = forms.CharField(widget=forms.PasswordInput,label="Confirm Password")
