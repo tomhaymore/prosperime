159d004c337b2f6f993a2db2a562055bf095e24d
Thomas Haymore <thomas.haymore@gmail.com>
2012-12-03 19:17:21 -0800

    -- added signal listener to create profile upon creation of user account

--- prosperime/accounts/models.py
+++ prosperime/accounts/models.py
@@ -4,6 +4,7 @@ import datetime
 # Django
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
 	user = models.OneToOneField(User)
@@ -25,3 +26,9 @@ class Account(models.Model):
     # returns name
     def __unicode__(self):
         return self.service
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
