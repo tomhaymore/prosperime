5059e5f69e64fd222a45a65579a5233500fef310
Thomas Haymore <thomas.haymore@gmail.com>
2012-12-06 22:24:13 -0800

    -- added model for images, updated organization profile model

--- prosperime/entities/models.py
+++ prosperime/entities/models.py
@@ -4,44 +4,58 @@ from django.contrib.auth.models import User, Account
 
 class Entity(models.Model):
 	
-	# returns path for uploading logos
-	def _getLogoPath(self,filename):
-		path = "logos/" + self.full_name + "/" + filename
-		return path
 
-	full_name = models.CharField(max_length=250)
-	first_name = models.CharField(max_length=150)
-	middle_name = models.CharField(max_length=150)
-	last_name = models.CharField(max_length=150)
+	name = models.CharField(max_length=450)
 	type = models.CharField(max_length=25) # org or person
 	subtype = models.CharField(max_length=50,blank=True,null=True) # firm, professional services, etc.
 	summary = models.TextField(blank=True,null=True) # quick 1-2 sentence overview of entity
 	description = models.TextField(blank=True,null=True) # more comprehensive overview
-	url = models.URLField(blank=True,null=True)
+	web_url = models.URLField(blank=True,null=True)
+	blog_url = models.URLField(blank=True,null=True)
 	twitter_handle = models.CharField(max_length=250,blank=True,null=True)
 	aliases = models.TextField(blank=True,null=True)
 	domain = models.CharField(max_length=250,blank=True,null=True)
 	founded_date = models.DateTimeField(blank=True,null=True)
 	deadpooled_date = models.DateTimeField(blank=True,null=True)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
 	cb_type = models.CharField(max_length=25,blank=True,null=True)
 	cb_updated = models.DateTimeField(blank=True,null=True)
-	cb_permalink = models.CharField(max_length=250,blank=True,null=True)
-	cb_url = models.URLField(blank=True,null=True)
-	logo = models.ImageField(max_length=450,upload_to=_getLogoPath,null=True)
-	logo_cb_attribution = models.CharField(max_length=450,null=True)
+	cb_permalink = models.CharField(max_length=250,blank=True,null=True) # Cruncbhase permalink
+	cb_url = models.URLField(blank=True,null=True) # Crunchbase URL
+	li_uniq_id = models.CharField(max_length=150,null=True) # LinkedIn unique id
+	li_univ_name = models.CharField(max_length=450,null=True) # LinkedIn universal name 
+	li_type = models.CharField(max_length=15,null=True)	
 	total_money = models.CharField(max_length=15,null=True,blank=True)
 	rels = models.ManyToManyField('self',symmetrical=False,through="Relationship") # for advising, employment relationships 
 	no_employees = models.IntegerField(blank=True,null=True)
+	size_range = models.CharField(max_length=2,null=True)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=15,default="active")
 	
 	# returns name
 	def __unicode__(self):
-		return self.full_name
+		return self.name
 
-	# returns name
-	def name(self):
-		return self.full_name
+class Image(models.Model):
+	
+	# returns path for uploading logos
+	def _getLogoPath(self,filename):
+		path = "logos/" + self.full_name + "/" + filename
+		return path
+
+	entity = models.ForeignKey(Entity)
+	logo = models.ImageField(max_length=450,upload_to=_getLogoPath)
+	source = models.CharField(max_length=450,null=True)
+	logo_type = models.CharField(max_length=15,null=True)
+	description = models.TextField(null=True)
+	license = models.TextField(null=True)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=15,default="active")
+
+	def __unicode__(self):
+		# concatenate entity name, description and logo
+		return " ".join([self.entity.name,self.description,'logo'])
 
 class Position(models.Model):
 	entity = models.ForeignKey(Entity)
@@ -53,6 +67,9 @@ class Position(models.Model):
 	start_date = models.DateField(null=True)
 	end_date = models.DateField(null=True)
 	status = models.CharField(max_length=15,default="active")
+
+	def __unicode__(self):
+		return self.title + " at " + self.entity.name
 
 class Relationship(models.Model):
 	entity1 = models.ForeignKey(Entity,related_name="entity1")
@@ -80,6 +97,7 @@ class Investment(models.Model):
 class Office(models.Model):
 	entity = models.ForeignKey(Entity)
 	description = models.CharField(max_length=450,blank=True,null=True)
+	is_hq = models.BooleanField(null=True)
 	addr_1 = models.CharField(max_length=150,blank=True,null=True)
 	addr_2 = models.CharField(max_length=150,blank=True,null=True)
 	addr_3 = models.CharField(max_length=150,blank=True,null=True)
