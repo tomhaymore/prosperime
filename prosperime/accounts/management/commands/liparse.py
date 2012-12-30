92d90fa48ac92416531f27b7d8f8245383bae54d
Thomas Haymore <thomas.haymore@gmail.com>
2012-12-29 20:05:02 -0800

    -- added public page parsing logic for user accounts

--- prosperime/accounts/management/commands/liparse.py
+++ prosperime/accounts/management/commands/liparse.py
@@ -4,9 +4,12 @@ import cgi
 from datetime import datetime, time
 import urllib2
 import os
+import re
 from math import ceil
 from optparse import make_option
 import urlparse
+from bs4 import BeautifulSoup
+from _retry import retry
 
 # from Django
 from django.utils import simplejson
@@ -146,7 +149,8 @@ class Command(BaseCommand):
 		acct.owner = user
 		acct.service = 'linkedin'
 		acct.uniq_id = user_info['id']
-		acct.public_url = user_info['publicProfileUrl']
+		if 'publicProfileUrl' in user_info:
+			acct.public_url = user_info['publicProfileUrl']
 		acct.status = "unlinked"
 		acct.save()
 
@@ -186,16 +190,20 @@ class Command(BaseCommand):
 			# check for month value
 			if 'month' in data['startDate']:
 				start_date = datetime.strptime(str(data['startDate']['month'])+"/"+str(data['startDate']['year']),"%m/%Y")
-			else:
+			elif 'year' in data['startDate']:
 				start_date = datetime.strptime(str(data['startDate']['year']),"%Y")
+			else:
+				start_date = datetime.strptime(data['startDate'],"%Y-%m-%d")
 			pos.start_date = start_date
 		# check for end date
 		if 'endDate' in data:
 			# check for month value
 			if 'month' in data['endDate']:
 				end_date = datetime.strptime(str(data['endDate']['month'])+"/"+str(data['endDate']['year']),"%m/%Y")
-			else:
+			elif 'year' in data['endDate']:
 				end_date = datetime.strptime(str(data['endDate']['year']),"%Y")
+			else:
+				end_date = datetime.strptime(data['endDate'],"%Y-%m-%d")
 			pos.end_date = end_date
 		pos.save()
 
@@ -204,9 +212,9 @@ class Command(BaseCommand):
 		pos = Position.objects.get(entity=co,person=user,title=data['title'])
 		pos.summary = data['headline']
 		pos.description = data['summary']
-		pos.start_date = data['start-date']
-		pos.end_date = data['end-date']
-		pos.current = data['is-current']
+		pos.start_date = data['startDate']
+		pos.end_date = data['endDate']
+		pos.current = data['isCurrent']
 		pos.save()
 
 	def get_position(self,user,co,data):
@@ -226,7 +234,15 @@ class Command(BaseCommand):
 		# return company
 		return co
 
-	def get_co_li_profile(self,co_id):
+	def get_company_from_name(self,name):
+		try:
+			co = Entity.objects.get(li_univ_name=name)
+		except:
+			# if company doesn't exist, return None
+			return None
+		return co
+
+	def get_co_li_profile(self,co_id=None,name=None):
 		# get oauth credentials from account
 		access_token = self.get_access_token(self.acct_id)
 
@@ -240,7 +256,10 @@ class Command(BaseCommand):
 		client = oauth.Client(consumer, token)
 
 		# construct api url 
-		co_api_url = self.co_api_url + str(co_id) + ":"  + co_fields + "?format=json"
+		if name:
+			co_api_url = self.co_api_url + "universal-name=" + str(co_id) + ":"  + co_fields + "?format=json"
+		else:
+			co_api_url = self.co_api_url + str(co_id) + ":"  + co_fields + "?format=json"
 
 		print co_api_url
 
@@ -255,9 +274,12 @@ class Command(BaseCommand):
 			return None
 		return content
 
-	def add_company(self,id):
+	def add_company(self,id=None,name=None):
 		# get company profile from LinkedIn
-		data = self.get_co_li_profile(id)
+		if name:
+			data = self.get_co_li_profile(name=name)
+		else:
+			data = self.get_co_li_profile(co_id=id)
 		# if nothing returned from LI, return None
 		if data is None:
 			return None
@@ -274,7 +296,7 @@ class Command(BaseCommand):
 			co.li_type = data['companyType']['code']
 		if 'ticker' in data:
 			co.ticker = data['ticker']
-		if 'website-urlteUrl' in data:
+		if 'websiteUrl' in data:
 			co.web_url = data['websiteUrl']
 		# co.domain = data['industries']
 		# co.li_status = data['status']
@@ -398,7 +420,7 @@ class Command(BaseCommand):
 		cxn.service = "linkedin"
 		cxn.save()
 
-	def get_entity(self,id,type="school"):
+	def get_institution(self,id,type="school"):
 		try:
 			entity = Entity.objects.get(li_type="school",li_uniq_id=id)
 			return entity
@@ -407,8 +429,8 @@ class Command(BaseCommand):
 
 	def add_institution(self,data):
 		ed = Entity()
-		ed.name = data['schoolName']
-		ed.li_uniq_id = data['id']
+		ed.name = data['inst_name']
+		ed.li_uniq_id = data['inst_uniq_id']
 		ed.type = 'organization'
 		ed.sub_type = 'ed-institution'
 		ed.li_type = 'school'
@@ -441,6 +463,121 @@ class Command(BaseCommand):
 			pos.end_date = end_date
 		pos.save()
 
+	@retry(urllib2.URLError,delay=10)
+	def get_public_page(self,url):
+		return urllib2.urlopen(url)
+
+	def process_public_page(self,user,url):
+		# fetch html and soup it
+
+		html = self.get_public_page(url)
+		soup = BeautifulSoup(html)
+
+		# get all profile container divs
+		divs = soup.find_all("div","section",id=re.compile("^profile"))
+
+		# loop throuh each div
+		for d in divs:
+			# identify type
+			if d['id'] == 'profile-experience':
+				# extract position data
+				positions = self.extract_pos_from_public_page(d)
+				for p in positions:
+					# check to see if a co uniq was returned
+					if p['co_uniq_name'] is not None:
+						# check to see if new company
+						co = self.get_company_from_name(p['co_uniq_name'])
+						if co is None:
+							# add new company
+							co = self.add_company(name=p['co_uniq_name'])
+							# if it's a new company, position must be new as well
+							if co is not None:
+								self.add_position(user,co,p)
+						else:
+							# TODO update company
+							pos = self.get_position(user,co,p)
+							if pos is None:
+								self.add_position(user,co,p)
+							# else:
+							# 	self.update_position(user,co,p)
+			elif d['id'] == 'profile-education':
+				ed_positions = self.extract_ed_pos_from_public_page(d)
+				for p in ed_positions:
+					# check to see if new company
+					inst = self.get_institution(p['inst_uniq_id'])
+					if inst is None:
+						# add new company
+						inst = self.add_institution(p)
+						# if it's a new company, position must be new as well
+						if inst is not None:
+							self.add_position(user,inst,p)
+					else:
+						# TODO update company
+						pos = self.get_position(user,inst,p)
+						if pos is None:
+							self.add_ed_position(user,inst,p)
+						# else:
+						# 	self.update_position(user,inst,p)
+
+	def extract_pos_from_public_page(self,data):
+		# initialize positions array
+		positions = []
+		# get all position divs
+		raw_positions = data.find_all("div","position")
+		# loop through each position
+		for p in raw_positions:
+			# get title of position
+			title = p.find("div","postitle").span.contents[0]
+			# get unique name of company
+			co_uniq_name = p.find("a","company-profile-public")
+			if co_uniq_name:
+				co_uniq_name = co_uniq_name.get('href')
+				m = re.search("(?<=\/company\/)([\w-]*)",co_uniq_name)
+				co_uniq_name = m.group(0)
+				# get start and end dates
+				start_date = p.find("abbr","dtstart")
+				if start_date is not None:
+					start_date = start_date.get('title')
+				try:
+					end_date = p.find('abbr','dtstamp').get('title')
+					current = True
+				except:
+					current = False
+
+				try:
+					end_date = p.find("abbr","dtend").get("title")
+				except:
+					end_date = None
+				# get descriptions
+				try:
+					descr = p.find("p","description").contents[0]
+				except:
+					descr = None
+				# append to main positions array
+				positions.append({'title':title,'co_uniq_name':co_uniq_name,'start_date':start_date,'end_date':end_date,'summary':descr,'isCurrent':current})
+		return positions
+
+	def extract_ed_pos_from_public_page(self,data):
+		# initialize positions array
+		positions = []
+		# get all position divs
+		raw_positions = data.find_all("div","position")
+		# loop through each position
+		for p in raw_positions:
+			inst_uniq_id = p.get('id')
+			inst_name = p.h3.contents[0]
+			try:
+				degree = p.find("span","degree").contents[0]
+				
+			except:
+				degree = None
+			try:
+				major = p.find("span","major").contents[0]
+			except:
+				major = None
+			positions.append({'inst_uniq_id':inst_uniq_id,'inst_name':inst_name,'degree':degree,'major':major})
+		return positions
+
 	def process_connections(self,user_id,acct_id):
 		# set update flag
 		update = False
@@ -470,6 +607,7 @@ class Command(BaseCommand):
 					user = self.add_dormant_user(c)
 					self.add_connection(self.focal_user,user)
 					# self.stdout.write(user)
+					
 					# process education
 					# if 'values' in c['educations']:
 					# 	for e in c['educations']['values']:
@@ -487,7 +625,7 @@ class Command(BaseCommand):
 					# 				# add position
 					# 				self.add_ed_position(user,ed,e)
 
-					# process positions
+					# process positions from API
 					if 'values' in c['positions']:
 						for p in c['positions']['values']:
 
@@ -497,19 +635,20 @@ class Command(BaseCommand):
 								co = self.get_company(p['company']['id'])
 								if co is None:
 									# add new company
-									co = self.add_company(p['company']['id'])
+									co = self.add_company(id=p['company']['id'])
 									# if it's a new company, position must be new as well
 									if co is not None:
 										self.add_position(user,co,p)
 								else:
 									# TODO update company
 									pos = self.get_position(user,co,p)
-									if update == True and pos is None:
+									if pos is None:
 										self.add_position(user,co,p)
-									elif update == True:
-										self.update_position(pos,p)
-									else:
-										self.add_position(user,co,p)
+									# else:
+									# 	self.add_position(user,co,p)
+					# process public profile page
+					if 'publicProfileUrl' in c:
+						self.process_public_page(user,c['publicProfileUrl'])
 				else:
 					self.add_connection(self.focal_user,user)
 
