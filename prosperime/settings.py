43a2edec313e04ad9a8dfae5f5f6b1ac6cd3f477
Thomas Haymore <thomas.haymore@gmail.com>
2013-01-04 23:14:32 -0800

    -- added full static css url

--- prosperime/settings.py
+++ prosperime/settings.py
@@ -66,6 +66,8 @@ STATIC_ROOT = ''
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
+STATIC_CSS_URL = '127.0.0.1:8000/static/'
+
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
