30fbc058128a131f74d5256a07e3c891a74a6474
Thomas Haymore <thomas.haymore@gmail.com>
2013-01-09 21:26:43 -0800

    -- updated sys path in wsgi.py

--- prosperime/wsgi.py
+++ prosperime/wsgi.py
@@ -1,6 +1,6 @@
-import os
+import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prosperime.settings")
-
+sys.path.append('prosperime')
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
 import django.core.handlers.wsgi
