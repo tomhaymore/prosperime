4e33e5bca33ac5b3927751ab64594e6d3a63e3d5
Thomas Haymore <thomas.haymore@gmail.com>
2013-02-04 16:41:03 -0800

    -- fixed regex for discover routes

--- prosperime/urls.py
+++ prosperime/urls.py
@@ -8,7 +8,7 @@ from django.conf import settings
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'entities.views.home', name='home'),
-    url(r'^discover/','entities.views.discover'),
+    url(r'^discover/$','entities.views.discover'),
     url(r'^discover/career/(\d+)/$','entities.views.discover_career'),
     url(r'^search/','entities.views.search'),
     url(r'^companies/','entities.views.companies'),
