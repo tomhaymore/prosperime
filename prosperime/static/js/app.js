47438bbad4a8926f4c782dc0683d9cedc14e22d0
Thomas Haymore <thomas.haymore@gmail.com>
2013-04-02 19:24:01 -0700

    -- removed unused Rafael code

--- prosperime/static/js/app.js
+++ prosperime/static/js/app.js
@@ -3,33 +3,6 @@
 
 $(function(){
 
-
-	// window.archtype = Raphael($("#arc-container"), 150, 150);
-
-	// archtype.customAttributes.arc = function (xloc, yloc, value, total, R) {
-	//     var alpha = 360 / total * value,
-	//         a = (90 - alpha) * Math.PI / 180,
-	//         x = xloc + R * Math.cos(a),
-	//         y = yloc - R * Math.sin(a),
-	//         path;
-	//     if (total == value) {
-	//         path = [
-	//             ["M", xloc, yloc - R],
-	//             ["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
-	//         ];
-	//     } else {
-	//         path = [
-	//             ["M", xloc, yloc - R],
-	//             ["A", R, R, 0, +(alpha > 180), 1, x, y]
-	//         ];
-	//     }
-	//     return {
-	//         path: path
-	//     };
-	// };
-	// Models
-
-
 	//--------------//
 	// ** Models ** //
 	//--------------//
