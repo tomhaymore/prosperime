d8803df95441f0b03d06a3fae0ee70abb4f720ff
tchaymore <thomas.haymore@gmail.com>
2013-06-04 11:54:46 -0700

    -- added focal entity to "entity" key

--- prosperime/careers/positionlib.py
+++ prosperime/careers/positionlib.py
@@ -202,6 +202,8 @@ class IdealPositionBase(PositionBase):
 		
 		# entities_list = [{'name':e.name,'id':e.id,'descr':e.description,'no_employees':e.no_employees,'locations':[o.city for o in e.offices.all()]} for e in entities]
 		entities_list = [{'name':e.name,'id':e.id,'descr':e.description,'no_employees':e.no_employees} for e in entities]
+		focal_entity = ideal_pos.position.entity
+		entities_list.append({'name':focal_entity.name,'id':focal_entity.id,'descr':focal_entity.description,'no_employees':focal_entity.no_employees})
 
 		# print entities_list
 
