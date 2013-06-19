e7a3ca65b789515c94585affa34bee98b0a4fa8a
Clayton Holz <ctholz@gmail.com>
2013-06-18 21:47:52 -0700

    careers/views.py

--- prosperime/careers/views.py
+++ prosperime/careers/views.py
@@ -28,18 +28,18 @@ import social.feedlib as feedlib
 import utilities.helpers as helpers
 
 
-## Clayton - DEV
-# def single_major(request, major_id):
+# Clayton - DEV
+def single_major(request, major_id):
 
-# 	positions = Position.objects.filter(field="Mechanical Engineering")
+	positions = Position.objects.filter(field="Mechanical Engineering")
 
 
-# 	people = [{'name':p.person.profile.full_name(), 'profile_pic':p.person.profile.default_profile_pic()} for p in positions]
-# 	data = {
-# 		"people":json.dumps(people)
-# 	}
+	people = [{'name':p.person.profile.full_name(), 'profile_pic':p.person.profile.default_profile_pic()} for p in positions]
+	data = {
+		"people":json.dumps(people)
+	}
 
-# 	return render_to_response("careers/major_viz.html", data, context_instance=RequestContext(request))
+	return render_to_response("careers/major_viz.html", data, context_instance=RequestContext(request))
 
 
 ######################################################
