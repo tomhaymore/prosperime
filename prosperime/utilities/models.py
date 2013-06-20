009c1cc9974fc588acd47fe69c9c1a14b9f3b681
tchaymore <thomas.haymore@gmail.com>
2013-06-20 13:12:34 -0700

    -- added report model

--- prosperime/utilities/models.py
+++ prosperime/utilities/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from storages.backends.s3boto import S3BotoStorage
+import datetime
 
-# Create your models here.
+class Report(models.Model):
+
+	# returns path for uploading reports
+    def _get_report_path(self,filename):
+        path = "reports/" + self.category + "/" + filename + str(datetime.date.today())
+        return path
+
+	title = models.CharField(max_length=150)
+	category = models.CharField(max_length=150)
+	description = models.TextField(null=True,blank=True)
+	report = models.FileField(storage=S3BotoStorage(bucket="prosperme_reports"),upload_to=_get_report_path)
