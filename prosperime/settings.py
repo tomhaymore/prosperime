a18f8d81a480211238b984f36445555faa1f7ea0
tchaymore <thomas.haymore@gmail.com>
2013-06-25 20:22:52 -0700

    -- enabled autocommit

--- prosperime/settings.py
+++ prosperime/settings.py
@@ -41,6 +41,9 @@ DATABASES = {
         'PASSWORD': 'Kstar77!',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {
+            'autocommit': True,
+        }
     }
 }
 
@@ -189,11 +192,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
