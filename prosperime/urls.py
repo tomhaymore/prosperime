d8404df4b78271084196ae8b8a926def46427f86
tchaymore <thomas.haymore@gmail.com>
2013-07-03 12:20:48 -0700

    -- disabled /internships link

--- prosperime/urls.py
+++ prosperime/urls.py
@@ -47,7 +47,7 @@ urlpatterns = patterns('',
     url(r'^plan/$', 'careers.views.plan'),
     url(r'^plan/(\d+)/$', 'careers.views.plan'),
 
-    url(r'^internships/$', 'careers.views.internships'),
+    # url(r'^internships/$', 'careers.views.internships'),
 
     url(r'^personalize/$','accounts.views.personalize'),
 
