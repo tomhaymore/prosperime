b389bdb4a4b1a91908222655334020ac176f06a7
Clayton Holz <ctholz@gmail.com>
2013-07-30 22:17:04 -0700

    social/Profile V1 complete w/o API integration, pushing to Thomas for UI review

--- prosperime/static/js/timeline-viz.js
+++ prosperime/static/js/timeline-viz.js
@@ -6,7 +6,7 @@ var Timeline = (function() {
     /***********/
 
     // Constants
-    var offset = 25,
+    var offset = 50,
         textBox_offset_top = 150;
 
     // Instance Variables
@@ -20,7 +20,26 @@ var Timeline = (function() {
         month_offset,
         midline,
         permissions,
+        hidden_nodes = [],
         $el;
+
+
+    function updateNode(position, index) {
+        // get textBox in question
+        var textBox = $(".timeline-position[data-index='" + index + "']")
+        var old_x = parseInt($(textBox).attr("data-x")) + ($(textBox).width() / 2)
+
+        // init template
+        var template = _.template($("#timeline-position-fragment-template").html())
+        $(textBox).html(template(position))
+
+        // check for new 'x'
+        // if it fits, center div
+        var half_box = $(textBox).width() / 2
+        var x = (old_x - half_box > offset) ? old_x - half_box : old_x;
+        x = (old_x + half_box > w) ? old_x - half_box : x;
+        $(textBox).attr("data-x", x)
+    };
 
     function createLine() {
         // line attributes
@@ -32,9 +51,9 @@ var Timeline = (function() {
     function createNode(position, index) {
         // local attributes
         var color = "rgb(111,182,212)",
-            outer_r = 12,
+            outer_r = 10,
             inner_r = 2,
-            outer_dot_attr = {'stroke-width':'1', 'stroke':color, 'fill':color, 'fill-opacity':'.8' },
+            outer_dot_attr = {'stroke-width':'2', 'stroke':color, 'fill':color, 'fill-opacity':'.8' },
             inner_dot_attr = {'stroke-width':'1', 'stroke':color, 'fill':color},
             hidden_dot_attr = {'stroke-width':'1', 'stroke':"#27AE60", 'fill':"#27AE60", cursor:"pointer" },
             hidden_text_attr = {"cursor": "pointer", "font-family":"Helvetica", "font-size":"12pt", "stroke":"none", "fill":"#ECF0F1"};
@@ -48,7 +67,7 @@ var Timeline = (function() {
             .attr(outer_dot_attr)
             .data("index", index)
             .mouseover(nodeMouseover)
-            .mouseout(nodeMouseout)
+            // .mouseout(nodeMouseout)
 
     
         // create & hide mouseover node
@@ -58,7 +77,6 @@ var Timeline = (function() {
             .data("index", index)
             .mouseout(nodeMouseout)
 
-
         // create & hide mouseover text
         var hidden_text = paper.text(x, midline, "Edit")
             .attr(hidden_text_attr)
@@ -67,8 +85,8 @@ var Timeline = (function() {
 
         // only allow editing given proper permission
         if (permissions.indexOf("w") != -1) {
-            hidden_node.click(function(ev) { animateToEdit(ev, index, position) })
-            hidden_text.click(function(ev) { animateToEdit(ev, index, position) })
+            hidden_node.click(function(ev) { animateToEdit(ev, index) })
+            hidden_text.click(function(ev) { animateToEdit(ev, index) })
         }
 
         // set id on outer_node for hover event, hover_node for hover event
@@ -76,6 +94,8 @@ var Timeline = (function() {
         outer_node.data("hidden_text_id", hidden_text.id)
         hidden_node.data("hidden_text_id", hidden_text.id)
 
+        // append hidden_node_id to g
+        hidden_nodes.push(hidden_node.id)
          
         // // create inner node (purely aesthetic)
         // var inner_node = paper.circle(x, midline, inner_r)
@@ -102,6 +122,28 @@ var Timeline = (function() {
             .css("top", textBox_offset_top + "px").css("left", "-500px")
     };
 
+    // runs through all nodes and triggers mouseexit events if needed
+    function checkAllNodes() {
+        for (i in hidden_nodes) checkSingleNode(paper.getById(hidden_nodes[i]))
+    };
+    
+    // check if the node is in the "hovered" state (=expanded to bigger radius)
+        // if so, returns node to normal and moves textBox offscreen
+    function checkSingleNode(hidden_node) {
+        if (hidden_node.attr("r") > 12) {
+            // move box back offscreen
+            $(".timeline-position[data-index='" + hidden_node.data("index") + "']").fadeOut(100).css("left", "-500px").show().removeClass("onscreen")
+
+            // hide hover node & text
+            if (permissions.indexOf("w") != -1 ) {
+                hidden_node.animate({"r":12}, "150", function() { this.hide() });
+                paper.getById(hidden_node.data("hidden_text_id")).hide()
+            } else {
+                hidden_node.animate({"r":12}, 250)
+            }   
+        };
+    };
+
     // triggered on SVG circles (=this)
     function nodeMouseover(e) {
 
@@ -110,6 +152,9 @@ var Timeline = (function() {
         // make sure not already triggered
         if ($(textBox).hasClass("onscreen"))
             return false;
+
+        // HACK: check that other nodes aren't already fired (from quick mousing)
+        checkAllNodes()
 
         // get 'x' value for textbox, move it to new location
         var x = parseInt($(textBox).attr("data-x"))
@@ -143,7 +188,7 @@ var Timeline = (function() {
         }
     }
 
-    function animateToEdit(ev, index, position) {
+    function animateToEdit(ev, index) {
 
         var textBox = $(".timeline-position[data-index='" + index + "']")
 
@@ -152,7 +197,7 @@ var Timeline = (function() {
             // hide textBox
             $(textBox).fadeOut(200).css("left", "-500px").css("top", textBox_offset_top + "px").show()
             // show form 
-            renderEditForm(position) // code in [profile.html]
+            renderEditForm(positions[index]) // code in [profile.html]
         })
 
     };
@@ -202,8 +247,16 @@ var Timeline = (function() {
 
         },
 
-        editPosition: function(index) {
-
+        editPosition: function(edited_position) {
+            // grab edited position
+            for (p in positions) {
+                if (edited_position.id == positions[p].id) {
+                    positions[p] = edited_position
+                    console.log(positions[p])
+                    updateNode(edited_position, p)
+                    break;
+                }
+            }
         },
 
         addPosition: function(position) {
@@ -211,6 +264,11 @@ var Timeline = (function() {
             console.log(positions[0])
             console.log(position)
             createNode(position, positions.length - 1)
+        },
+
+        // Returns Timeline to original state (no events firing or fired)
+        normalize: function() {
+            checkAllNodes()
         },
     }
 }());
