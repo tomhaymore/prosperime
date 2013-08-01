bc57da1c6bafdb320e7a52ad8875b9063620566c
tchaymore <thomas.haymore@gmail.com>
2013-08-01 15:40:27 -0700

    -- new form for advisor requests

--- prosperime/social/forms.py
+++ prosperime/social/forms.py
@@ -1,8 +1,9 @@
 # from Django
 from django import forms
+from django.contrib.auth.models import User
 
 # from prosperime
-from social.models import Tag
+from social.models import Conversation,Tag
 
 class RecruiterInterestForm(forms.Form):
 
@@ -13,3 +14,13 @@ class ConversationForm(forms.Form):
 	name = forms.CharField(label="Title")
 	summary = forms.CharField(label="Summary")
 	tags = forms.ModelMultipleChoiceField(required=False,queryset=Tag.objects.all())
+
+class AskAdvisorForm(forms.Form):
+
+	user = forms.ModelChoiceField(queryset=User.objects.filter(profile__status="active"))
+	advisor = forms.ModelChoiceField(queryset=User.objects.filter(profile__status="active"))
+	subject = forms.CharField(label="Subject")
+	body = forms.CharField(label="Body")
+	method = forms.CharField(label="Method")
+	question = forms.ModelChoiceField(queryset=Conversation.objects.all())
+
